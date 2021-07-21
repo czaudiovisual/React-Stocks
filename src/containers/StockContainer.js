ea60902afe57f8ea6b0eb5d7be781529f6c32a03
Christian Bermeo <czaudiovisual@hotmail.com>
2021-07-21 11:23:13 -0400

    rendering stocks on the stockComponent

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
+  // initial state
+  state = {
+    stocks: [],
+  };
 
+  // fetch request to access the data and parsed to stocks
+  componentDidMount() {
+    fetch("http://localhost:3001/stocks")
+      .then(res => res.json())
+      .then((stocks) => this.setState({ stocks }))
+  };
+
+  // render the stock component and mapping over each stock
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {//render the list of stocks here passing props 
+          this.state.stocks.map((stock) => (
+            <Stock
+              name={stock.name}
+              ticker={stock.ticker}
+              price={stock.price} />
+          ))}
       </div>
     );
   }
